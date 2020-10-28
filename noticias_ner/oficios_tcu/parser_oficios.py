9de38df6bfea2e8f1880bd5d70afc3a41875c042
Monique <moniquebm@tcu.gov.br>
2020-10-28 12:02:25 -0300

    parser de ofícios

--- noticias_ner/oficios_tcu/parser_oficios.py
+++ noticias_ner/oficios_tcu/parser_oficios.py
@@ -11,13 +11,15 @@ def parse_oficio(arquivo, ner=FinedTunedBERT_NER()):
     print('Executando o parsing do arquivo ' + caminho)
     nome_arquivo = caminho[caminho.rfind(os.sep):]
     texto = open(caminho, 'r', encoding='utf-8').read()
+    regex_cpf_cnpj = r'([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?' \
+                     r'[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})'
 
     if 'Citação' in nome_arquivo:
-        expressoes_regulares = [r'Natureza: Citação(.*?)Prezada', r'Natureza: Citação(.*?)Prezado',
-                                r'Natureza: Citação(.*?)Senhora', r'Natureza: Citação(.*?)Senhor',
-                                r'Natureza: Citação(.*?)Senhor(a)',
-                                ]
-        for regex in expressoes_regulares:
+        regexes_enderecamento = [r'Natureza: Citação(.*?)Prezada', r'Natureza: Citação(.*?)Prezado',
+                                 r'Natureza: Citação(.*?)Senhora', r'Natureza: Citação(.*?)Senhor',
+                                 r'Natureza: Citação(.*?)Senhor(a)',
+                                 ]
+        for regex in regexes_enderecamento:
             ocorrencias = re.findall(regex, texto, flags=re.DOTALL)
             if len(ocorrencias) > 0:
                 enderecamento = ocorrencias[0]
@@ -29,17 +31,37 @@ def parse_oficio(arquivo, ner=FinedTunedBERT_NER()):
                 entidades_texto = ner._extrair_entidades_de_texto(enderecamento)
                 for entidade, tipo_entidade in entidades_texto:
                     if tipo_entidade == 'PESSOA':
+                        responsavel = entidade
                         # Busca por CPF
-                        regex_cpf_cnpj = r'([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?' \
-                                         r'[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})'
                         cpfs = re.findall(regex_cpf_cnpj, enderecamento, flags=re.DOTALL)
                         if len(cpfs) > 0:
-                            cpf = cpfs[0][1]
-                            print(cpf)
+                            cpf_cnpj = cpfs[0][1]
+                        break
+                    elif tipo_entidade == 'ORGANIZAÇÃO':
+                        responsavel = entidade
+
+                        # Busca por CNPJ
+                        cnpjs = re.findall(regex_cpf_cnpj, enderecamento, flags=re.DOTALL)
+                        if len(cnpjs) > 0:
+                            cpf_cnpj = cnpjs[0][0]
+                        break
+
                 break
 
+        regexes_irregularidades = [r'O débito é decorrente de(.*?):', r'O débito é decorrente do(.*?):',
+                                   r'O débito é decorrente da(.*?):', r'O débito é decorrente dos(.*?):',
+                                   r'O débito é decorrente das(.*?):']
+        for regex in regexes_irregularidades:
+            ocorrencias = re.findall(regex, texto, flags=re.DOTALL)
+            if len(ocorrencias) > 0:
+                irregularidades = ocorrencias[0]
+
+                if isinstance(irregularidades, tuple):
+                    irregularidades = irregularidades[0]
     elif 'Oitiva' in nome_arquivo:
-        pass
+        return None
+
+    return responsavel, cpf_cnpj, irregularidades.strip()
 
 
 if __name__ == '__main__':
@@ -56,4 +78,4 @@ if __name__ == '__main__':
 
     for root, subdirs, files in os.walk(diretorio_txt):
         for file in files:
-            parse_oficio(arquivo=diretorio_txt.joinpath(file))
+            print(parse_oficio(arquivo=diretorio_txt.joinpath(file)))
