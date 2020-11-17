2e4263f60df1feb239d2016f3094bcdeda919c41
Monique <moniquebm@tcu.gov.br>
2020-11-17 18:49:26 -0300

    ajuste

--- noticias_ner/api/wsgi.py
+++ noticias_ner/api/wsgi.py
@@ -1,3 +1,10 @@
+# Adiciona diretorio raiz ao PATH. Devido a ausência de setup.py, isto garante que as importações sempre funcionarão
+import os
+import sys
+
+diretorio_raiz = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir)
+sys.path.append(diretorio_raiz)
+
 from noticias_ner.api.app import app
 
 if __name__ == '__main__':
