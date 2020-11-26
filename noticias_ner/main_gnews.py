803d826078f1dec6a1b029bcf6b6e2ad86353689
Monique <moniquelouise@gmail.com>
2020-11-26 17:39:27 -0300

    comentando temporariamente o envio de email para teste no servidor

--- noticias_ner/main_gnews.py
+++ noticias_ner/main_gnews.py
@@ -52,5 +52,5 @@ if __name__ == '__main__':
         # Receita Federal
         arquivo_final = identificar_possiveis_empresas_citadas(os.path.join(config.diretorio_dados, 'ner.xlsx'),
                                                                filtrar_por_empresas_unicas=True)
-        __enviar_email_com_resultados([(arquivo_entidades, 'ner.xlsx'), (arquivo_final, 'com_empresas.xlsx')],
-                                      dia_inicio)
+        # __enviar_email_com_resultados([(arquivo_entidades, 'ner.xlsx'), (arquivo_final, 'com_empresas.xlsx')],
+        #                               dia_inicio)
